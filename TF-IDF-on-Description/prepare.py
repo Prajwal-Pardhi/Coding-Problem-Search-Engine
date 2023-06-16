0801dfc83354030ea0076b71fb5e43f4f32b1a65
Prajwal-Pardhi <prajwalpardhi2003@gmail.com>
2023-06-16 23:58:39 +0530

    first commit

---
+++ TF-IDF-on-Description/prepare.py
@@ -0,0 +1,107 @@
+# read the index.txt and prepare documents, vocab , idf
+
+import chardet
+import re
+
+def find_encoding(fname):
+    try:
+        with open(fname, 'rb') as f:
+            result = chardet.detect(f.read())
+            charenc = result['encoding']
+            return charenc
+    except UnicodeDecodeError:
+        return 'utf-8'
+
+filename = 'G:\Algozenith\Web Dev\AZ-Hackathon\Project\Leetcode-Que-Scrapper\Qdata\index.txt'
+my_encoding = find_encoding(filename)
+
+with open(filename, 'r', encoding=my_encoding) as f:
+    lines = f.readlines()
+    print("length of lines: ",len(lines))
+    #print(lines)
+
+def preprocess(document_text):
+    # remove the leading numbers from the string, remove not alpha numeric characters, make everything lowercase
+    #terms = [term.lower() for term in document_text.strip().split()[1:] if term.isalnum()]
+    #splits the document at each space and removes 1st split
+
+    terms = []
+    for term in document_text.strip().split()[1:]:
+        term = re.sub(r'\W+', '', term.lower())
+        if term.isalnum() and term != " ":
+            terms.append(term)
+    return terms
+
+vocab = {} #dictionery of unique words(key) and their freq(value)
+documents = []
+for index, line in enumerate(lines):
+    # read statement and add it to the line and then preprocess
+    #tokens = preprocess(line)
+
+    
+    file_path = f'G:\Algozenith\Web Dev\AZ-Hackathon\Project\Leetcode-Que-Scrapper\Qdata\data\{index+1}\{index+1}.txt'
+    
+    my_encoding2 = find_encoding(file_path)    
+    with open(file_path,'r', encoding= my_encoding2 ) as file:
+        file_contents = ''
+        for line2 in file:
+            if 'Example 1:' in line2:
+                break
+            file_contents += line2
+        #print(line + file_contents)
+        tokens = preprocess(line + file_contents)    
+        #print(tokens)
+
+    documents.append(tokens)
+    #print("Documents :",documents)
+
+    tokens = set(tokens) # freq added by "1 for each line" if word present once or many no. of time hence taken as set
+
+    for token in tokens: 
+        if token not in vocab:
+            vocab[token] = 1
+        else:
+            vocab[token] += 1
+    print(index, 'done!!')
+
+# reverse sort the vocab by the values
+vocab = dict(sorted(vocab.items(), key=lambda item: item[1], reverse=True))
+
+print('Number of documents: ', len(documents))
+print('Size of vocab: ', len(vocab))
+print('Sample document: ', documents[0])
+
+# save the vocab in a text file
+with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\TF-IDF\\tf-idf-data\\vocab.txt', 'w') as f:
+    for key in vocab.keys():
+        f.write("%s\n" % key)
+        
+    print("vocab file created!!")
+
+# save the idf values in a text file
+with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\TF-IDF\\tf-idf-data\idf-values.txt', 'w') as f:
+    for key in vocab.keys():
+        f.write("%s\n" % vocab[key])
+    print("idf-values file created!!")
+
+# save the documents in a text file
+with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\TF-IDF\\tf-idf-data\documents.txt', 'w') as f:
+    for document in documents:
+        f.write("%s\n" % ' '.join(document))
+    print("documents file created!!")
+
+
+inverted_index = {}
+for index, document in enumerate(documents):
+    for token in document:
+        if token not in inverted_index:
+            inverted_index[token] = [index]
+        else:
+            inverted_index[token].append(index)
+
+# save the inverted index in a text file
+with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\TF-IDF\\tf-idf-data\inverted-index.txt', 'w') as f:
+    for key in inverted_index.keys():
+        f.write("%s\n" % key)
+        f.write("%s\n" % ' '.join([str(doc_id) for doc_id in inverted_index[key]]))
+    print("inverted index file creared!!")
---
+++ TF-IDF-on-Description/query.py
@@ -0,0 +1,153 @@
+import math
+from flask import Flask, render_template
+from flask import jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import SubmitField
+
+app = Flask(__name__ , static_url_path='/static')
+
+def load_vocab():
+    vocab = {}
+    with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\TF-IDF-on-Description\\tf-idf-data\\vocab.txt', 'r') as f:
+        vocab_terms = f.readlines()
+    with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\TF-IDF-on-Description\\tf-idf-data\idf-values.txt', 'r') as f:
+        idf_values = f.readlines()
+    
+    for (term,idf_value) in zip(vocab_terms, idf_values):
+        vocab[term.strip()] = int(idf_value.strip())
+    
+    return vocab
+
+def load_documents():
+    documents = []
+    with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\TF-IDF-on-Description\\tf-idf-data\documents.txt', 'r') as f:
+        documents = f.readlines()
+    documents = [document.strip().split() for document in documents]
+
+    #print('Number of documents: ', len(documents))
+    #print('Sample document: ', documents[0])
+    return documents
+
+def load_inverted_index():
+    inverted_index = {}
+    with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\TF-IDF-on-Description\\tf-idf-data\inverted-index.txt', 'r') as f:
+        inverted_index_terms = f.readlines()
+
+    for row_num in range(0,len(inverted_index_terms),2):
+        term = inverted_index_terms[row_num].strip()
+        documents = inverted_index_terms[row_num+1].strip().split()
+        inverted_index[term] = documents
+    
+    #print('Size of inverted index: ', len(inverted_index))
+    return inverted_index
+
+def load_Qindex():
+    q_links = []
+    with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\Leetcode-Que-Scrapper\Qdata\Qindex.txt', 'r') as f:
+        q_links = f.readlines()
+    #q_links = [each_link.strip().split() for each_link in q_links]
+    return q_links
+
+def load_index():
+    q_headings = []
+    with open('G:\Algozenith\Web Dev\AZ-Hackathon\Project\Leetcode-Que-Scrapper\Qdata\index.txt', 'r') as f:
+        q_headings = f.readlines()
+    #q_headings = [each_head.strip().split()[1:] for each_head in q_headings]
+
+    return q_headings
+
+
+vocab_idf_values = load_vocab() #dict (word: no. of documents in which 'word' is present)
+documents = load_documents() #list [['a','b',...for each document1],['a2','b2',...for docu2],[...],...]
+inverted_index = load_inverted_index() #dict (word:[indexes of documents in which it present])
+que_links = load_Qindex() #list of question links
+que_heading = load_index() #list of question headings
+
+
+def get_tf_dictionary(term):
+    tf_values = {} #dict {document index : frequency of term in that document/toal no. of words in that document}
+    if term in inverted_index:
+        for document in inverted_index[term]:
+            if document not in tf_values:
+                tf_values[document] = 1
+            else:
+                tf_values[document] += 1
+                
+    for document in tf_values:
+        tf_values[document] /= len(documents[int(document)]) #def of tf value
+    
+    return tf_values
+
+def get_idf_value(term):
+    return math.log(len(documents)/vocab_idf_values[term]) #def of idf value
+
+def calculate_sorted_order_of_documents(query_terms):
+    potential_documents = {}
+    result = []
+
+    for term in query_terms:
+        if term not in vocab_idf_values:
+            continue
+        tf_values_by_document = get_tf_dictionary(term)
+        idf_value = get_idf_value(term)
+        #print(term,tf_values_by_document,idf_value)
+        for document in tf_values_by_document:
+            if document not in potential_documents:
+                potential_documents[document] = tf_values_by_document[document] * idf_value
+            else: potential_documents[document] += tf_values_by_document[document] * idf_value
+    len(potential_documents)
+    if(len(potential_documents) == 0):
+        print("No matching question found, try some relevent words")
+        return []
+    
+    #print(potential_documents)
+    # divite by the length of the query terms
+    for document in potential_documents:
+        potential_documents[document] /= len(query_terms)
+
+    potential_documents = dict(sorted(potential_documents.items(), key=lambda item: item[1], reverse=True))
+
+
+    for document_index in potential_documents:
+        #print('Document: ', documents[int(document_index)], ' Score: ', potential_documents[document_index])
+        #print('Document Heading: ', que_heading[int(document_index)],
+        # 'Document link: ',que_links[int(document_index)], 
+        # ' Score: ', potential_documents[document_index])
+        heading = que_heading[int(document_index)].split()
+        new_heading = ' '.join(heading[1:])
+
+        result.append( {"Question heading": new_heading ,
+                        "Question link": que_links[int(document_index)], 
+                        "Score":potential_documents[document_index] } )
+        
+    return result[:20:]
+
+#query_string = input('Enter your query: ')
+#query_terms = [term.lower() for term in query_string.strip().split()]
+
+#print(query_terms)
+#result = calculate_sorted_order_of_documents(query_terms)
+#print(result)
+
+app.config['SECRET_KEY'] = 'prajwal_key'
+
+class SearchForm(FlaskForm):
+    search = StringField('Enter your search term')
+    submit = SubmitField('Search')
+
+@app.route("/<query>")
+def return_links(query):
+    q_terms = [term.lower() for term in query.strip().split()]
+    return jsonify(calculate_sorted_order_of_documents(q_terms)[:20:])
+ 
+@app.route("/",methods = ['GET', 'POST'])
+def home():
+    form = SearchForm()
+    results = []
+    if form.validate_on_submit():
+        query_string = form.search.data
+        query_terms = [term.lower() for term in query_string.strip().split()] 
+        results = calculate_sorted_order_of_documents(query_terms)[:20:]
+        #print(results)
+    return render_template('index.html',form = form, results = results )
