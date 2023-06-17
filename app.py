a56e8ef27558169b64c8378644572dc3a7b35f1c
Prajwal Pardhi <106570642+Prajwal-Pardhi@users.noreply.github.com>
2023-06-17 17:36:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app.template_folder = 'templates'
 
 def load_vocab():
     vocab = {}
-    with open('TF-IDF\\tf-idf-data\\vocab.txt', 'r') as f:
+    with open('TF-IDF\tf-idf-data\vocab.txt', 'r') as f:
         vocab_terms = f.readlines()
-    with open('TF-IDF\\tf-idf-data\idf-values.txt', 'r') as f:
+    with open('TF-IDF\tf-idf-data\idf-values.txt', 'r') as f:
         idf_values = f.readlines()
     
     for (term,idf_value) in zip(vocab_terms, idf_values):
@@ -22,7 +22,7 @@ def load_vocab():
 
 def load_documents():
     documents = []
-    with open('TF-IDF\\tf-idf-data\documents.txt', 'r') as f:
+    with open('TF-IDF\tf-idf-data\documents.txt', 'r') as f:
         documents = f.readlines()
     documents = [document.strip().split() for document in documents]
 
@@ -32,7 +32,7 @@ def load_documents():
 
 def load_inverted_index():
     inverted_index = {}
-    with open('TF-IDF\\tf-idf-data\inverted-index.txt', 'r') as f:
+    with open('TF-IDF\tf-idf-data\inverted-index.txt', 'r') as f:
         inverted_index_terms = f.readlines()
 
     for row_num in range(0,len(inverted_index_terms),2):
